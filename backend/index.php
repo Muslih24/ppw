55543961c5666483323828bf8c2c1b3171350ef8
ptar22 <67457733+ptar22@users.noreply.github.com>
2020-06-29 15:16:55 +0700

    tes

---
+++ backend/index.php
@@ -0,0 +1,47 @@
+<?php 
+require 'functions.php';
+  
+$user = query("SELECT * FROM user");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<table border="1" cellpadding="10" cellspacing="0">
+		<tr>
+			<th>No</th>
+			<th>Username</th>
+			<th>Hak Akses</th>
+			<th>Nama</th>
+			<th>Jenis Kelamin</th>
+			<th>Tanggal Lahir</th>
+			<th>Alamat</th>
+			<th>No HP</th>
+			<th>E-mail</th>
+			<th>Aksi</th>
+		</tr>
+		<?php $i = 1; ?>
+		<?php foreach ($user as $row) :	?> 
+
+		<tr>
+			<td><?= $i; ?></td>
+			<td><?= $row["username"]  ?></td>
+			<td><?= $row["hak_akses"]  ?></td>
+			<td><?= $row["nama"]  ?></td>
+			<td><?= $row["jk"]  ?></td>
+			<td><?= $row["tanggal_lahir"]  ?></td>
+			<td><?= $row["alamat"]  ?></td>
+			<td><?= $row["no_hp"]  ?></td>
+			<td><?= $row["email"]  ?></td>
+			<td>
+				<a href="">Edit</a> |
+				<a href="">Delete</a>
+			</td>
+		</tr>
+	<?php $i++; ?>
+	<?php endforeach; ?>
+	</table>
+</body>
+</html>
