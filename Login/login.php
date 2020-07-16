fb49c8899ca67c27483dcaea6fd28c81e2a287a3
Efrizaaa <67216080+Efrizaaa@users.noreply.github.com>
2020-07-16 15:28:09 +0700

    Login

---
+++ Login/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>::Login::</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="fontawesome/css/fontawesome.css">
+	<link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+	<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@600&display=swap" rel="stylesheet">
+</head>
+<body>
+
+	<div class="loginbox">
+		<h1>LOGIN</h1>
+
+		<div class="textbox" style="color: #373737">
+			<i class="far fa-user"></i>
+			<input type="text" name="" placeholder="Username">
+		</div>
+
+		<div class="textbox" style="color: #373737">
+			<i class="fas fa-lock"></i>
+			<input type="Password" name="" placeholder="Password">
+		</div>
+
+		<input type="submit" name="" value="Login">
+		
+	</div>
+
+</body>
+</html>
