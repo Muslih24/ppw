9deb62644d5c64d79f1730f62245e31134176cf1
Muslih24 <Muslih259@gmail.com>
2020-08-09 16:11:59 +0700

    qweqwe

--- backend/admin/kategori/deletekategori.php
+++ backend/admin/kategori/deletekategori.php
@@ -8,24 +8,18 @@ if (!$_SESSION["hak_akses"]=="superadmin") {
 
 $id_kategori= $_GET["id_kategori"];
 
-$pilih = mysqli_query("SELECT * FROM kategori WHERE id_kategori=''$id_kategori'");
-$data = mysqli_fetch_array($pilih);
-$foto = $data['foto_kategori'];
-unlink("../../../assets/img/images/kategori/".$foto);
-$hapus = mysqli_query("DELETE from kategori where id_kategori='$id_kategori'");
-var_dump($hapus);die;
-if ($hapus) {
+if (deletek($id_kategori) > 0 ) {
 		echo "
 			<script>
 				alert('Successed To Delete');
-				document.location.href = 'index_kategori.php';
+				document.location.href = 'index_admin.php';
 			</script>
 		";
 	}else{
 		echo "
 			<script>
 			alert('Failed To Delete');
-				document.location.href = 'index_kategori.php';
+				document.location.href = 'index_admin.php';
 			</script>
 		";
 	}
